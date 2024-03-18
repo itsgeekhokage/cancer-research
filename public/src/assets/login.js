367747adaa51e0429bf9c178e3db1fe9bdb12196
Shivam Prajapati <shivam.prajapati.phe21@itbhu.ac.in>
2024-03-19 00:00:28 +0530

    loginsupdate

--- public/src/assets/login.js
+++ public/src/assets/login.js
@@ -1,20 +1,36 @@
 const logins = [
     {
-        username : "shivam",
-        password : "shivam.ca"
+        username: "shivam",
+        password: "shivam.ca"
     },
     {
-        username : "harsh",
-        password : "harsh.ca"
+        username: "harsh",
+        password: "harsh.ca"
     },
     {
-        username : "nitish",
-        password : "nitish.ca"
+        username: "nitish",
+        password: "nitish.ca"
     },
     {
-        username : "admin",
-        password : "admin.ca"
+        username: "admin",
+        password: "admin.ca"
     },
+    {
+        username: "Aditya12",
+        password: "Aditya@#12"
+    },
+    {
+        username: "Jatasya30",
+        password: "jatasya123"
+    },
+    {
+        username: "Hinal",
+        password: "hinalexplo"
+    },
+    {
+        username: "aditya.choudhari16",
+        password: "IIT-103"
+    }
 ]
 
 export default logins;
