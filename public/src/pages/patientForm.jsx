60ded95f08751d6a30adddb4cfc89f76a3795b16
Shivam Prajapati <shivam.prajapati.phe21@itbhu.ac.in>
2024-04-01 23:23:21 +0530

    backdrop

--- public/src/pages/patientForm.jsx
+++ public/src/pages/patientForm.jsx
@@ -6,6 +6,7 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import typesOfCancer from "../assets/typesOfCancer";
 import districts from "../assets/district";
+import SimpleBackdrop from "./Backdrop";
 
 const PatientForm = () => {
   const [formData, setFormData] = useState({
@@ -22,6 +23,8 @@ const PatientForm = () => {
     pip: "",
   });
   const [districtOptions, setDistrictOptions] = useState([]);
+  const [open, setOpen] = useState(false);
+
   const location = useLocation();
   const navigate = useNavigate();
 
@@ -55,6 +58,7 @@ const PatientForm = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(formData);
+    setOpen(true);
     setPatientForm();
   };
 
@@ -88,186 +92,191 @@ const PatientForm = () => {
         pip: location.state,
       });
       toast.success("Patient data submitted successfully");
+      setOpen(false);
     } catch (error) {
       console.error("There was a problem with your fetch operation:", error);
       toast.error("unsuccessful, check your data again...");
+      setOpen(false);
     }
   };
 
   return (
-    <div className={styles.formContainer}>
-      <h2>Enter Patient Data</h2>
-      <form onSubmit={handleSubmit}>
-        <div className={styles.formGroup}>
-          <label htmlFor="serialno">Serial No:</label>
-          <input
-            type="text"
-            id="serialno"
-            name="serialno"
-            value={formData.serialno}
-            onChange={handleChange}
-            placeholder="Enter Serial No"
-            required
-          />
-        </div>
-        <div className={styles.formGroup}>
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            placeholder="Enter Name"
-            required
-          />
-        </div>
-        <div className={styles.formGroup}>
-          <label htmlFor="age">Age:</label>
-          <input
-            type="number"
-            id="age"
-            name="age"
-            value={formData.age}
-            onChange={handleChange}
-            placeholder="Enter Age"
-            required
-          />
-        </div>
-        <div className={styles.formOptionGroup}>
-          <label>Sex:</label>
-          <div>
-            <label>
-              <input
-                type="radio"
-                name="sex"
-                value="male"
-                checked={formData.sex === "male"}
-                onChange={handleChange}
-              />
-              Male
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="sex"
-                value="female"
-                checked={formData.sex === "female"}
-                onChange={handleChange}
-              />
-              Female
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="sex"
-                value="others"
-                checked={formData.sex === "others"}
-                onChange={handleChange}
-              />
-              Others
-            </label>
+    <>
+      <SimpleBackdrop open={open} />
+      <div className={styles.formContainer}>
+        <h2>Enter Patient Data</h2>
+        <form onSubmit={handleSubmit}>
+          <div className={styles.formGroup}>
+            <label htmlFor="serialno">Serial No:</label>
+            <input
+              type="text"
+              id="serialno"
+              name="serialno"
+              value={formData.serialno}
+              onChange={handleChange}
+              placeholder="Enter Serial No"
+              required
+            />
           </div>
-        </div>
-        <div className={styles.formGroup}>
-          <label htmlFor="fatherName">Father/Husband Name:</label>
-          <input
-            type="text"
-            id="fatherName"
-            name="fatherName"
-            value={formData.fatherName}
-            onChange={handleChange}
-            placeholder="Enter Father's Name"
-            required
-          />
-        </div>
-        <div className={styles.formGroup}>
-          <label htmlFor="state">State:</label>
-          <select
-            id="state"
-            name="state"
-            value={formData.state}
-            onChange={handleChange}
-            required>
-            <option value="">Select State</option>
-            {Object.keys(districts).map((state, index) => (
-              <option
-                key={index}
-                value={state}>
-                {state}
-              </option>
-            ))}
-          </select>
-        </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              placeholder="Enter Name"
+              required
+            />
+          </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="age">Age:</label>
+            <input
+              type="number"
+              id="age"
+              name="age"
+              value={formData.age}
+              onChange={handleChange}
+              placeholder="Enter Age"
+              required
+            />
+          </div>
+          <div className={styles.formOptionGroup}>
+            <label>Sex:</label>
+            <div>
+              <label>
+                <input
+                  type="radio"
+                  name="sex"
+                  value="male"
+                  checked={formData.sex === "male"}
+                  onChange={handleChange}
+                />
+                Male
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  name="sex"
+                  value="female"
+                  checked={formData.sex === "female"}
+                  onChange={handleChange}
+                />
+                Female
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  name="sex"
+                  value="others"
+                  checked={formData.sex === "others"}
+                  onChange={handleChange}
+                />
+                Others
+              </label>
+            </div>
+          </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="fatherName">Father/Husband Name:</label>
+            <input
+              type="text"
+              id="fatherName"
+              name="fatherName"
+              value={formData.fatherName}
+              onChange={handleChange}
+              placeholder="Enter Father's Name"
+              required
+            />
+          </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="state">State:</label>
+            <select
+              id="state"
+              name="state"
+              value={formData.state}
+              onChange={handleChange}
+              required>
+              <option value="">Select State</option>
+              {Object.keys(districts).map((state, index) => (
+                <option
+                  key={index}
+                  value={state}>
+                  {state}
+                </option>
+              ))}
+            </select>
+          </div>
 
-        <div className={styles.formGroup}>
-          <label htmlFor="district">District:</label>
-          <select
-            id="district"
-            name="district"
-            value={formData.district}
-            onChange={handleChange}
-            required>
-            <option value="">Select District</option>
-            {districtOptions.map((district, index) => (
-              <option
-                key={index}
-                value={district}>
-                {district}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className={styles.formGroup}>
-          <label htmlFor="post">Post:</label>
-          <input
-            type="text"
-            id="post"
-            name="post"
-            value={formData.post}
-            onChange={handleChange}
-            placeholder="Enter Post"
-            required
-          />
-        </div>
-        <div className={styles.formGroup}>
-          <label htmlFor="village">Village:</label>
-          <input
-            type="text"
-            id="village"
-            name="village"
-            value={formData.village}
-            onChange={handleChange}
-            placeholder="Enter Village"
-            required
-          />
-        </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="district">District:</label>
+            <select
+              id="district"
+              name="district"
+              value={formData.district}
+              onChange={handleChange}
+              required>
+              <option value="">Select District</option>
+              {districtOptions.map((district, index) => (
+                <option
+                  key={index}
+                  value={district}>
+                  {district}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="post">Post:</label>
+            <input
+              type="text"
+              id="post"
+              name="post"
+              value={formData.post}
+              onChange={handleChange}
+              placeholder="Enter Post"
+              required
+            />
+          </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="village">Village:</label>
+            <input
+              type="text"
+              id="village"
+              name="village"
+              value={formData.village}
+              onChange={handleChange}
+              placeholder="Enter Village"
+              required
+            />
+          </div>
 
-        <div className={styles.formGroup}>
-          <label htmlFor="typeOfTumor">Type of Tumor:</label>
-          <select
-            id="typeOfTumor"
-            name="typeOfTumor"
-            value={formData.typeOfTumor}
-            onChange={handleChange}
-            required>
-            <option value="">Select Type of Tumor</option>
-            {typesOfCancer.map((cancerType, index) => (
-              <option
-                key={index}
-                value={cancerType}>
-                {cancerType}
-              </option>
-            ))}
-          </select>
-        </div>
-        <button
-          type="submit"
-          className={styles.button}>
-          Submit
-        </button>
-      </form>
-    </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="typeOfTumor">Type of Tumor:</label>
+            <select
+              id="typeOfTumor"
+              name="typeOfTumor"
+              value={formData.typeOfTumor}
+              onChange={handleChange}
+              required>
+              <option value="">Select Type of Tumor</option>
+              {typesOfCancer.map((cancerType, index) => (
+                <option
+                  key={index}
+                  value={cancerType}>
+                  {cancerType}
+                </option>
+              ))}
+            </select>
+          </div>
+          <button
+            type="submit"
+            className={styles.button}>
+            Submit
+          </button>
+        </form>
+      </div>
+    </>
   );
 };
 
