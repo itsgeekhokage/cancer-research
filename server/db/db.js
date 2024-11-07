6636b43f0fb311f8189bdad3e0d40a9449367c8e
Shivam Prajapati <100841132+itsgeekhokage@users.noreply.github.com>
2024-11-07 23:04:23 +0530

    Update db.js

--- server/db/db.js
+++ server/db/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDb = async (DATABASE_URL) => {
     try {
         await mongoose.connect(DATABASE_URL, {
-            dbName : "cancerProject",
+            dbName : "dummyCancerProject",
         });
         console.log("database connected successfully....");
     } catch (error) {
